b9b843df35b34b0b01b0979f1f69803a31e36f5b
ArmandoAnescu <121225784+ArmandoAnescu@users.noreply.github.com>
2025-05-08 12:05:01 +0200

    aa

--- index.php
+++ index.php
@@ -1,145 +1,21 @@
 <?php
-echo "ciao";
-$myvar="sono una variabile";
-$myvar=5;
-echo '<br>'.$myvar;
-// debolmente tipizzato, non devo dichiarare il tipo delle variabili
-// posso etterci informazioni diverse di tipo diverso
-
-$a=0;
-$b='0';
-if($a===$b){//fa il type coercion, quindi il casting
-    echo 'sono uguali';
-}else{
-    echo "<br>"."non sono uguali";
-}
-//operatore di identità guarda il valore e il tipo
-//altri operatori
-//isset(nomeVar)- empty(nomeVar)-isnull()
-//isset se istanziata
-//is empty se vuota
-//is null se il valore è null
-$vuoto=0;
-$var4=5;
-$nnullo=null;
-if(isset($var4)){
-    echo '<br>esiste';
-}
-if(empty($vuoto)){
-    echo '<br>vuota';
-}
-if(is_null($nnullo)){
-    echo '<br> è nulla';
-}
-echo is_null($vuoto);
-echo is_null($var4);
-if(3.5==='3.5'){
-    echo '<br> uguali';
-}
-if(true==='false'){
-    echo "<br> è vero";
-}
-if(0==" "){
-    echo "<br> è vero";
-}
-//iteration structures:do while - while -for -break and continue
-//selection structures if-if else - switch
-//
-//match
-$grade='j';
-$message=match($grade){
-    'A'=>'letter a',
-    'B'=>'letter b',
-    'C','D'=> 'letter c or d',
-    default =>'other letters'
-};
-echo '<br>'.$message;
-$subtotal=200;
-$total=0;
-$total=match(true){
-    $subtotal<=200 =>$total=$subtotal*0.9,
-    $subtotal>200 =>$total=$subtotal*0.8,
-};
-echo '<br>'.$total;
-//conditional operator
-$num1=200;
-$num2=100;
-$num1>$num2?$r='ok':$r='ko';
-echo '<br>'.$r;
-$null0=0;
-//coalescing operator
-$num3=$null0??$num2;//se la prima è nulla lui prende la seconda altrimenti la prima
-echo '<br>'.$num3;
-//spaceship operator
-echo $num1<=>$num2;//se il sx < -1 se = 0 se > 1
-/*
- * strings
- * array
- * classes
- * datetime
- * importing files
- * ecc
- *
- * */
-// strings
-/*
- *
- * */
-
-$language='PHP';
-$message='<br> welcome to '.$language;
-echo $message;
-$message2=" <br> welcome to $language";//interpolazione i "" vanno ad intercettare e riconoscere le variabili
-echo $message2;
-$count=12;
-$item='flower';
-$message4="<br> you have $count ${item}s <br>";//puoi isolare la var con { }
-echo $message4;
-$message5=<<< MESSAGE
-Cominato è molto bello,patatoso,chupaposo,dingoso e molto feinoso ragazzo
-un po biricchino e diddyino.
-Angelo è molto sigma.▲◄19ƒ6╠æå4H7╩42♣4◘♦8•4◘•♀TD6»♦4@654 46♦65♦64♠4♠4141♠♦64♠4☺442`
-MESSAGE;
-echo $message5.'<br>';
-/*
- * funzioni da saper
- * strlen()
- * substring()
- * substr_replace()
- * trim()
- * ltrim() rtrim
- * stripslashes()
- * str_pad()
- * strpos()
- * strtpos()
- * stripos()
- * str_contains()
- * str_starts_with()
- * str_end_with()
- * strToUpper()
- * strToLower()
- * ucfirst()
- * ucwords()
- * strrev()
- * str_shuffle()
- * str_repeat()
- * str_replace()
- * strcmp()
- * strcasecmp()
- * strnstcmp()
- * explode()
- * implode()
- * chr() - ord()
- * */
-
-
-
-
-
-
-
-
-
-
-
-
+require 'vendor/tecnickcom/tcpdf/tcpdf.php';
+$name='5f';
+$lastname='Viola Marchesini';
+$pdf=new TCPDF();
+$pdf->AddPage();//aggiungo una pagina al pdf
+$pdf->setFillColor(179,179,179);
+$pdf->Rect(0,0,210,297,'F');
+$pdf->setTextColor(3,86,252);
+$pdf->setFont('Helvetica','',18);
+$pdf->Cell(0,10,'Hello this is your ticket',0,1,'C');//riga del pdf
+$pdf->Ln(80);
+$pdf->setTextColor(252,3,61);
+$pdf->setFont('Helvetica','',14);
+$pdf->Cell(0,10,"Name: {$name} ",0,1,'L');//riga del pdf
+$pdf->Cell(0,10,"Last name: {$lastname} ",0,1,'L');//riga del pdf
+$pdf->Cell(0,10,"Data e ora evento: ".date('d/m/y H:i'),0,1,'L');//riga del pdf
+$pdf->write2DBarcode("Ciao:{$name} {$lastname} - Uffizi di Firenze data:".date('d/m/y H:i'),'QRCODE,L',10,50,50,50,[],'N');
+$pdf->Line(10,10,$pdf->getPageWidth()-10,10);
+$pdf->Image('logo.jpg',$pdf->getPageWidth()-70,$pdf->getPageHeight()-250,50,50);
+$pdf->Output('Ticket.pdf','I');//crea il pdf
